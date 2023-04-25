6034a1e5442e88ccc71f1814629241b0c279a4ae
Danny Lin <danny.l@ljit.io>
2023-04-25 17:40:03 +0800

    New project

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function FirstPost() {
+	return (
+		<>
+			<Head>
+				<title>First Post</title>
+				<script src="https://connect.facebook.net/en_US/sdk.js" />
+			</Head>
+
+			<h1>First Post</h1>
+
+			<h2>
+				<Link href="/">Back to home</Link>
+			</h2>
+		</>
+	);
+}
