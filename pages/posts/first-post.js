52e87203cc04c166ce448c65f7869f78e1876b14
Danny Lin <danny.l@ljit.io>
2023-04-26 16:43:53 +0800

    Update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
 
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
 	return (
-		<>
+		<Layout>
 			<Head>
 				<title>First Post</title>
 				<script src='https://connect.facebook.net/en_US/sdk.js' />
@@ -23,6 +25,6 @@ export default function FirstPost() {
 			<h2>
 				<Link href='/'>Back to home</Link>
 			</h2>
-		</>
+		</Layout>
 	);
 }
