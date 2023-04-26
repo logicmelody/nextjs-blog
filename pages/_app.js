2f62fefcf89500bdd66ec7e737501818d38108b5
Danny Lin <danny.l@ljit.io>
2023-04-26 17:36:40 +0800

    Update

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+/*
+根據官方文件:
+
+you can add global CSS files by importing them from pages/_app.js. You cannot import global CSS anywhere else.
+*/
+import '../styles/global.css';
+
+export default function App({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
