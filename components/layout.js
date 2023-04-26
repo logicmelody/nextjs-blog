6b7bd913207e975d7986018722e2dad9ec566071
Danny Lin <danny.l@ljit.io>
2023-04-26 17:07:40 +0800

    Update

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,5 @@
+// 有辦法 disable CSS Module
+// https://stackoverflow.com/questions/67934463/how-to-turn-off-css-module-feature-in-next-js
 import styles from './layout.module.css';
 
 export default function Layout({ children }) {
