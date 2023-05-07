29f0a645c866d8a50bad916925ce12890a9186d4
Danny Lin <danny.l@ljit.io>
2023-05-07 12:42:38 +0800

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,11 @@ you can add global CSS files by importing them from pages/_app.js. You cannot im
 */
 import '../styles/global.css';
 
+/*
+The Component prop is the active page, so whenever you navigate between routes, Component will change to the new page. Therefore, any props you send to Component will be received by the page.
+
+pageProps is an object with the initial props that were preloaded for your page by one of our data fetching methods, otherwise it's an empty object.
+*/
 // <Component {...pageProps} /> 可以看成 {renderSwitches(routes)}
 export default function App({ Component, pageProps }) {
 	const ComponentElement = (
