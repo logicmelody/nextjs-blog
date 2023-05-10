b2ec9f5791e057fc2f1915458943b263f5362aca
Danny Lin <danny.l@ljit.io>
2023-05-10 14:39:46 +0800

    Update

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+/*
+// Default Document
+import { Html, Head, Main, NextScript } from 'next/document';
+ 
+export default function Document() {
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
+*/
+function Document() {
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
+
+export default Document;
