9b37e52659a14c5cda6f7d8ad824a3d4ec2111a4
Danny Lin <danny.l@ljit.io>
2023-05-10 14:55:41 +0800

    Update

---
+++ pages/404/index.js
@@ -0,0 +1,11 @@
+import styles from './404.module.css';
+
+function Custom404() {
+	return (
+		<div className={styles.container}>
+			<h1>404 - Page Not Found</h1>
+		</div>
+	);
+}
+
+export default Custom404;
